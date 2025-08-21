dbc43a28665809dbeba465d71f97708bc15a9699
JADundee <jadundee@icloud.com>
2025-08-21 14:04:22 -0600

    updated cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
     'https://forum-ahrf.onrender.com',
-    'https://forum-ahrf.onrender.com/*',
 ]
 
 module.exports = allowedOrigins
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,9 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
+        console.log("Allowed Origins:", allowedOrigins);
+        console.log("Request Origin:", origin);
+
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
